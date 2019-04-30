16cfe512a27ccf7f346efb6369c0f307a1534f40
Julian Theis <j.theis@tu-bs.de>
2019-04-30 12:03:34 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setuptools.setup(name='kdl_parser_jvytee',
+                 version='0.1',
+                 author='Julian Theis',
+                 author_email='jlntheis@gmail.com',
+                 description='Create PyKDL chains from URDF robot descriptions',
+                 long_description=long_description,
+                 long_description_content_type='test/markdown',
+                 url='https://github.com/jvytee/kdl_parser',
+                 packages=setuptools.find_packages(),
+                 classifiers=['Programming Language :: Python :: 3',
+                              'License :: OSI Approved :: BSD License',
+                              'Operating System :: OS Independent'])
